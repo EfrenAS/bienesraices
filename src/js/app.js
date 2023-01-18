5aed9a6568e71d815b93572ba2943c418a123e5d
Efren <histrix.dev@gmail.com>
2023-01-17 22:27:30 -0600

    Addedd -Dark mode- to site

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   eventListeners();
+  activeDarkMode();
 });
 
 function eventListeners() {
@@ -12,5 +13,12 @@ function showMenuResposive() {
   const navigation = document.querySelector('.navigation-main');
 
   navigation.classList.toggle('display-on');
-  
+}
+
+function activeDarkMode () {
+  const darkModebtn = document.querySelector('.dark-mode-btn');
+
+  darkModebtn.addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+  })
 }
