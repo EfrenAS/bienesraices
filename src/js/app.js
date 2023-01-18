2d0ab344d8b09691da93b91bf9f8489f22327238
Efren <histrix.dev@gmail.com>
2023-01-17 23:02:06 -0600

    Added Dark Mode to site through preferences system of user

--- src/js/app.js
+++ src/js/app.js
@@ -16,7 +16,26 @@ function showMenuResposive() {
 }
 
 function activeDarkMode () {
+
   const darkModebtn = document.querySelector('.dark-mode-btn');
+  const isSystemDarkMode = window.matchMedia('prefers-color-scheme: dark');
+
+  if (isSystemDarkMode.matches) {
+    document.body.classList.add('dark-mode');
+  } else {
+    document.body.classList.remove('dark-mode');
+    console.log('Todo bien');
+  }
+
+
+  isSystemDarkMode.addEventListener('change', function() {
+    if (isSystemDarkMode.matches) {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+      //console.log('Todo bien');
+    } 
+  });
 
   darkModebtn.addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
